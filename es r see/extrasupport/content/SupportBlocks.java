09d462e1640414507bc1fabbbbbd17dbddaafc29
Kingfrankbob <67441018+Kingfrankbob@users.noreply.github.com>
2021-11-16 10:31:29 -0600

    Update SupportBlocks.java

--- es r see/extrasupport/content/SupportBlocks.java
+++ es r see/extrasupport/content/SupportBlocks.java
@@ -1,5 +1,9 @@
-package extranilla.content;
+package extrasupport.content;
 
+import extrasupport.entities.bullet.*;
+import extrasupport.world.blocks.defense.*;
+import extrasupport.world.blocks.defense.turrets.*;
+import extrasupport.world.blocks.defense.turrets.pattern.*;
 import mindustry.*;
 import mindustry.content.*;
 import mindustry.ctype.*;
@@ -18,7 +22,9 @@ import mindustry.world.blocks.storage.*;
 import mindustry.world.draw.*;
 import mindustry.world.meta.*;
 
-public class NillaBlocks implements ContentList {
+public class SupportBlocks implements ContentList {
+  
+  compounce, coalPress, 
   
   
   
@@ -28,17 +34,16 @@ public class NillaBlocks implements ContentList {
           compounce = new GenericCrafter("Compounce"){{
             requirements(Category.crafting, with(Items.copper, 90, Items.lead, 60, Items.scrap, 45, Items.scrap, 45, Items.titanium, 45));
             craftEffect = Fx.smeltsmoke; //todo
-            outputItem = new ItemStack(Items.metaglass, 2);
+            outputItem = new ItemStack(Items.blast-compound, 2);
             craftTime = 80f;
             size = 2;
             hasPower = hasItems = true;
-            drawer = new DrawSmelter(Pal.lancerLaser.cpy());
             ambientSound = Sounds.smelter;
             ambientSoundVolume = 0.07f;
             baseEfficiency = 0f;
-            attribute = MindyAttribute.metallic;
 
-            consumes.items(with(Items.sand, 2));
+            consumes.items(with(Items.scrap, 10));
+            consumes.liquids(with(Liquids.water, 12));
             consumes.power(0.60f);
         }};
 
