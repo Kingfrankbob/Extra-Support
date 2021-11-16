f9384cb1317c8608a114f101e216fb133367701e
Kingfrankbob <67441018+Kingfrankbob@users.noreply.github.com>
2021-11-16 11:14:53 -0600

    Update and rename es r see/extrasupport/extrasupport.java to src/extrasupport/extrasupport.java

---
+++ src/extrasupport/extrasupport.java
@@ -0,0 +1,33 @@
+ 
+package extrasupport.extrasupport;
+
+
+import arc.*;
+import arc.func.*;
+import arc.struct.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.ctype.*;
+import mindustry.game.EventType.*;
+import mindustry.game.Team;
+import mindustry.mod.*;
+import mindustry.mod.Mods.*;
+import extrasupport.content.*;
+import mindustry.net.*;
+import mindustry.type.*;
+import mindustry.ui.*;
+import mindustry.world.*;
+import mindustry.world.blocks.*;
+
+import static java.lang.Float.*;
+import static mindustry.Vars.*;
+public class ExtraSupport extends Mod{
+
+    @Override
+    public void init(){
+    }
+
+    @Override
+    public void loadContent(){
+    }
+}
